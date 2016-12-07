4766be0306bf0405b61834a8e3c273e8a3baadc1
ZaiZu <zaizu3000@gmail.com>
2016-12-07 17:02:22 +0300

    scripts: lastfm: enhancements

--- scripts/lastfm/getlastfmalbumimg.sh
+++ scripts/lastfm/getlastfmalbumimg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl "http://www.last.fm/ru/music/""$1""/+albums" |grep "$2" |grep "img class"| awk -F'"' '{ print $4}'
+curl "http://www.last.fm/ru/music/""$1""/+albums" |grep -i "$2" |grep "img class"| awk -F'"' '{ print $4}'
--- scripts/lastfm/showmocpimg.sh
+++ scripts/lastfm/showmocpimg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-feh $(./getlastfmalbumimg.sh "$(echo "$(mocp -Q %artist)")" "$(mocp -Q %album)")
+feh $(./getlastfmalbumimg.sh "$(echo "$(mocp -Q %artist)")" "$(echo "$(mocp -Q %album)")")
