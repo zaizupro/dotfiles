121618ada33c3c627d834944bba8c8ae2253dd77
ZaiZu <zaizu3000@gmail.com>
2017-10-13 18:50:12 +0300

    scripts: pasta fix

--- scripts/pasta.sh
+++ scripts/pasta.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-nano /tmp/curl_buffer && curl -F 'sprunge=<-' http://sprunge.us < /tmp/curl_buffer  && rm /tmp/curl_buffer
+#nano /tmp/curl_buffer && curl -F 'sprunge=<-' http://sprunge.us < /tmp/curl_buffer  && rm /tmp/curl_buffer
+
+nano /tmp/curl_buffer && nc termbin.com 9999 < /tmp/curl_buffer  && rm /tmp/curl_buffer
+
