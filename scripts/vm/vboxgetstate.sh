38987fc27f7d9670d3fbe93ed4f4a28e2b6d13d8
ZaiZu <zaizu3000@gmail.com>
2018-04-13 17:04:44 +0300

    scripts: vbox getstate colorizided

--- scripts/vm/vboxgetstate.sh
+++ scripts/vm/vboxgetstate.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
+YELLOWFGBG=$(tput setab 94;tput setaf 220)
+REDFGBG=$(tput setab 52;tput setaf 196)
+GREENFGBG=$(tput setab 58;tput setaf 154)
+NC=$(tput sgr0)
+
+
 VMNAME=${1}
 VMSTATE=$(VBoxManage showvminfo ${VMNAME} --machinereadable | grep VMState= | cut -d '"' -f 2)
 
-echo "$(tput setab 58;tput setaf 154)$VMNAME is $VMSTATE"
+case "${VMSTATE}" in 
+    aborted)
+    COLOR="${REDFGBG}"
+    ;;
+    saved)
+    COLOR="${YELLOWFGBG}"
+    ;;
+    poweroff)
+    COLOR="${NC}"
+    ;;
+    running)
+    COLOR="${GREENFGBG}"
+    ;;
+    *)
+    ;;
+esac
+
+echo "${COLOR}${VMNAME} is ${VMSTATE}${NC}"
