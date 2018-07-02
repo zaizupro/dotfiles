8c6da06f379fdf3d00b1e07773081648fe4af647
ZaiZu <zaizu3000@gmail.com>
2018-07-02 18:52:53 +0300

    scripts: refactoring and new colors for make output kolorizer

--- scripts/makek.sh
+++ scripts/makek.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+
+
 # Colored Make 2015.2.11
 # Copyright (c) 2014 Renato Silva
 # GNU GPLv2 licensed
 
 
 #extended by zaz 2015
+
+#==============COLORS============
+# add some color
+if [ -f ${HOME}/.bash_colors ]; then
+    source ${HOME}/.bash_colors
+fi
+
 
 # This script will colorize output of make and GCC so it becomes easier to
 # notice the errors and warnings in the complex build output.
@@ -31,18 +40,18 @@ fi
 normal="\e[0m"
 
 # Errors, warnings, notes and compiler recipes
-error="s/(^error|^.*[^a-z]error:)/$(printf $red)\\1$(printf $normal)/i"
+KEY="s/(\[-W.*\])/${YELLOWFGBG}\\1${NC}/i"
+error="s/(^.*)(error|[^a-z]error:|undefined reference to|[^a-z]undefined reference to)(.*)/${REDFG}\\1${NC}${REDFGBG}\2${REDFGBG}${YELLOW}\3${NC}/i"
 note="s/(^note|^.*[^a-z]note:)/$(printf $green)\\1$(printf $normal)/i"
 warning="s/(^warning|^.*[^a-z]warning:)/$(printf $yellow)\\1$(printf $normal)/i"
-make="s/^make(\[[0-9]+\])?:/$(printf $purple)make\\1:$(printf $normal)/"
+make="s/(^make(\[[0-9]+\])?:|.*Configuring project)(.*)/${PURPFGBG}\\1${NC}$(tput setaf 183)\3${NC}/"
 cxx="s/(std=c++)(\[[0-9]+\])?:/$(printf $purple)std=c++\\1:$(printf $normal)/"
 compiler_recipe="s/^(gcc(.exe)? .*)/$(printf $gray)\\1$(printf $normal)/"
-undefined_reference_to="s/(^undefined reference to|^.*[^a-z]undefined reference to)/$(printf $red)\\1$(printf $normal)/i"
-install="s/(^install|^.*[^a-z]install )/$(printf $green)\\1$(printf $normal)/i"
+install="s/(^install)(.*)/${GREENFGBG}\\1${NC}${GREEN}\2${NC})/i"
 
 if [[ $(uname -or) != 1.*Msys ]]; then
-    command make "$@" 2> >(sed -r -e "$warning" -e "$error" -e "$note" -e "$undefined_reference_to" -e "$cxx" -e "$make" -e "$compiler_recipe" -e"$install") \
-                       > >(sed -r -e "$warning" -e "$error" -e "$note" -e "$undefined_reference_to" -e "$cxx" -e "$make" -e "$compiler_recipe" -e"$install")
+    command make "$@" 2> >(sed -r -e "$warning" -e "$error" -e "$note" -e "$undefined_reference_to" -e "$cxx" -e "$make" -e "$compiler_recipe" -e"$install" -e "$KEY") \
+                       > >(sed -r -e "$warning" -e "$error" -e "$note" -e "$undefined_reference_to" -e "$cxx" -e "$make" -e "$compiler_recipe" -e"$install" -e "$KEY")
 else
     # MinGW MSYS does not support process substitution
     command make "$@" 2>&1 | sed -r -e "$warning" -e "$error" -e "$make" -e "$compiler_recipe"
