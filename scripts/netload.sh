64536b74dc32c1afc3cdeb50ec478230817341d8
ZaiZu <zaizu3000@gmail.com>
2019-08-27 12:08:31 +0300

    scripts: netload enhancements and improvements

--- scripts/netload.sh
+++ scripts/netload.sh
@@ -1,30 +1,70 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+    echo
+    echo "    usage: $0 network-interface"
+    echo
+    ARR=($(ip a  | grep -o  "^[0-9]*:\ \w*" | cut -d ' ' -f 2 | uniq))
+    echo "    e.g.:"
+    echo "         $(basename $0) ${ARR[$i]}"
+    echo
+    echo "network-interfaces:"
+    for i in "${!ARR[@]}"; do
+        echo -e "    ${ARR[$i]}"
+    done
+    exit
+fi
 
-#case "$OSTYPE" in
-#    linux-gnu)
-        if [ -z "$1" ]; then
-            echo
-            echo usage: $0 network-interface
-            echo
-            echo e.g. $0 eth0
-            echo
-            exit
+
+X_TYPE="all"
+VERBOSITY=false
+
+if [ ! -z "${2}" ]; then
+    if [ "${2}" == "rx" ] || [ "${2}" == "tx" ]; then
+        X_TYPE=${2}
+        if [ ! -z "${3}" ]; then
+            if [ "${3}" == "-v" ]; then
+                VERBOSITY=true
+            fi
         fi
+    fi
 
-        IF=$1
+    if [ "${2}" == "-v" ]; then
+        VERBOSITY=true
+    fi
+fi
 
-        R1=`cat /sys/class/net/$1/statistics/rx_bytes`
-        T1=`cat /sys/class/net/$1/statistics/tx_bytes`
-        sleep 1
-        R2=`cat /sys/class/net/$1/statistics/rx_bytes`
-        T2=`cat /sys/class/net/$1/statistics/tx_bytes`
-        TBPS=`expr $T2 - $T1`
-        RBPS=`expr $R2 - $R1`
-        TKBPS=`expr $TBPS / 1024`
-        RKBPS=`expr $RBPS / 1024`
-        printf "%s:rx%d tx%d " $1 $RKBPS $TKBPS
+IF=$1
 
-#        ;;
-#esac
+## TODO check $1 in ARR[@]
+R1=$(cat /sys/class/net/$1/statistics/rx_bytes)
+T1=$(cat /sys/class/net/$1/statistics/tx_bytes)
+sleep 1
+
+if [ "$X_TYPE" == "rx" ] || [ "$X_TYPE" == "all" ]; then
+    R2=$(cat /sys/class/net/$1/statistics/rx_bytes)
+    RBPS=$(expr $R2 - $R1)
+    RKBPS=$(expr $RBPS / 1024)
+fi
+
+if [ "$X_TYPE" == "tx" ] || [ "$X_TYPE" == "all" ]; then
+    T2=$(cat /sys/class/net/$1/statistics/tx_bytes)
+    TBPS=$(expr $T2 - $T1)
+    TKBPS=$(expr $TBPS / 1024)
+fi
+
+if [ "$X_TYPE" == "rx" ] || [ "$X_TYPE" == "all" ]; then
+
+    if [ "${VERBOSITY}" == "true" ]; then
+        PREFIX_X_TYPE="rx "
+    fi
+    # printf "%s%d \n" "$PREFIX_X_TYPE" "$RKBPS"
+    printf "%s%d \n" "$PREFIX_X_TYPE" "$RBPS"
+fi
+if [ "$X_TYPE" == "tx" ] || [ "$X_TYPE" == "all" ]; then
+    # printf "%s%d \n" "$PREFIX_X_TYPE" "$TKBPS"
+    printf "%s%d \n" "$PREFIX_X_TYPE" "$TBPS"
+fi
+
+
 
